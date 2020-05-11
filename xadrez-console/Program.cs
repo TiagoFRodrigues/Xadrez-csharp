405bd6d502cb1b39d433c457816bfba291fc7539
TiagoFRodrigues <tfr_ch@hotmail.com>
2020-05-11 08:41:02 -0300

    melhorando a implementacao do metodo imprimir tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -14,8 +14,10 @@ namespace xadrez_console
 
                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                
                 Tela.ImprimirTabuleiro(tab);
             }
             catch (TabuleiroException e)
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -11,6 +11,9 @@ namespace xadrez_console
         {
             for (int l =0; l < tabuleiro.Linhas; l++)
             {
+
+                Console.Write(8 - l + " ");
+
                 for (int c = 0; c < tabuleiro.Colunas; c++)
                 {
                     if (tabuleiro.Peca(l,c) == null)
@@ -19,11 +22,31 @@ namespace xadrez_console
                     }
                     else
                     {
-                        Console.Write(tabuleiro.Peca(l, c) + " ");
+                        ImprimirPeca(tabuleiro.Peca(l, c));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+
+            Console.WriteLine("  a b c d e f g h ");
         }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+
+        }
+
     }
 }
