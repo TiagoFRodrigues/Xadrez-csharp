81196cd78f8d0a67838e0bcdb59eb8c12a84cc9c
TiagoFRodrigues <tfr_ch@hotmail.com>
2020-05-11 07:33:06 -0300

    Comecando a implementar o tabuleiro

---
+++ xadrez-console/Tabuleiro/Cor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez_console.tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amrela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja,
+        Roxa
+    }
+}
