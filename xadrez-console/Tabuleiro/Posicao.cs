0d13b24043a3d17724f72795d68be91830954498
TiagoFRodrigues <tfr_ch@hotmail.com>
2020-05-11 08:27:45 -0300

    Criando classe posicao xadrez

--- xadrez-console/Tabuleiro/Posicao.cs
+++ xadrez-console/Tabuleiro/Posicao.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace xadrez_console.tabuleiro
 {
     class Posicao
--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using xadrez_console.tabuleiro;
+﻿using xadrez_console.tabuleiro;
 
 namespace xadrez_console.xadrez
 {
