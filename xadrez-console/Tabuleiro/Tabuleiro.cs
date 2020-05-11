326c77eb83e2052998fb973308c99bab9b80d839
TiagoFRodrigues <tfr_ch@hotmail.com>
2020-05-11 07:46:41 -0300

    Comecando a implementar o metodo que imprime o tabuleiro

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -16,5 +16,11 @@ namespace xadrez_console.tabuleiro
             Colunas = colunas;
             Pecas = new Peca[Linhas, Colunas];
         }
+
+        public Peca Peca(int linha, int coluna)
+        {
+            return Pecas[linha, coluna];
+        }
+
     }
 }
---
+++ xadrez-console/Tela.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using xadrez_console.tabuleiro;
+
+namespace xadrez_console
+{
+    class Tela
+    {
+        public static void ImprimirTabuleiro(Tabuleiro tabuleiro)
+        {
+            for (int l =0; l < tabuleiro.Linhas; l++)
+            {
+                for (int c = 0; c < tabuleiro.Colunas; c++)
+                {
+                    if (tabuleiro.Peca(l,c) == null)
+                    {
+                        Console.Write("_ ");
+                    }
+                    else
+                    {
+                        Console.Write(tabuleiro.Peca(l, c) + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
